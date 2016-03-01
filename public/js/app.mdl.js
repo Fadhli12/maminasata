1fb0c2f8fa5a0a238f99d757b7f5fe995bd57795
Fadhli12 <thegenesispath@gmail.com>
2016-03-01 11:12:06 +0800

    commit vcs.xml, workspace.xml + page koridor

--- public/js/app.mdl.js
+++ public/js/app.mdl.js
@@ -7,10 +7,15 @@ var myApp = angular.module('myApp',['ui.router','ngAnimate','ngRoute']);
 myApp.config(function($stateProvider,$urlRouterProvider,$locationProvider){
    $stateProvider
        .state('map',{
-           url:'/',
-           templateUrl:'public/map/view/map.html',
+           url:'/map/:id_koridor',
+           templateUrl:'public/app/view/map.html',
            controller:'mapCtrl'
+       })
+       .state('koridor',{
+           url:'/koridor',
+           templateUrl:'public/app/view/koridor.html',
+           controller:'koridorCtrl'
        });
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/map/3');
     $locationProvider.html5Mode({enabled:true,requireBase:true});
 })
--- public/js/global.ctrl.js
+++ public/js/global.ctrl.js
@@ -1,116 +1,13 @@
 /**
  * Created by Genesis on 03/02/2016.
  */
-myApp.controller('mapCtrl',function($scope,$http,$interval){
+myApp.controller('globalCtrl',function($scope,$http){
     $scope.koridor = [];
-    $http.get('public/map/model/getKoridor.php')
+    $http.get('public/app/model/getKoridor.php')
         .then(function(res){
             $scope.koridor = res.data
         },function(){
             console.log('error')
         });
 
-    //modal init
-    $(document).ready(function(){
-        // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-        $('.modal-trigger').leanModal();
-    });
-    $('#modal1').openModal();
-    $('#modal1').closeModal();
-
-
-
-    $scope.getRute = function(k){
-        $http.post('public/map/model/getRute.php',{koridor : k})
-            .then(function(res){
-                getMap(res.data);
-            },function(){
-                console.log('error');
-            });
-    };
-    $scope.getRute('1');
-    //$interval(getRute, 5000);
-
-    function getMap(rute){
-        var myOptions = {
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-        // init directions service
-        var dirService = new google.maps.DirectionsService();
-        var dirRenderer = new google.maps.DirectionsRenderer({suppressMarkers: true});
-        dirRenderer.setMap(map);
-
-        //google map
-        var request = {
-            origin: rute.direction[0].location,
-            destination: rute.direction[0].location,
-            waypoints: rute.direction,
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-        dirService.route(request, function(result, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                dirRenderer.setDirections(result);
-            }
-        });
-        /*var halte = [
-            {latitude:'-5.1666716',longitude:'119.4477935'},
-            {latitude:'-5.1567238',longitude:'119.4464341'},
-            {latitude:'-5.1415601',longitude:'119.4485844'}
-        ]*/
-
-        var halte = rute.halte;
-
-        var infoWindow = new google.maps.InfoWindow();
-        var createMarker = function (halte) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: new google.maps.LatLng(halte.latitude, halte.longitude),
-                title: halte.nama
-            });
-            marker.content = '' +
-                '<div class="infoWindowContent">' +
-                '   <h4>' + marker.title + '</h4>  ' +
-                '   <p><i class="fa fa-map-marker"></i> Lokasi :' + halte.latitude +','+ halte.longitude+'</p>' +
-                '</div>';
-            google.maps.event.addListener(marker, 'click', function () {
-                /*open map default content*/
-                infoWindow.setContent(marker.content);
-                infoWindow.open(map, marker);
-            });
-
-        };
-
-        //inisiasi marker
-        for (i = 0; i < halte.length; i++){
-            createMarker(halte[i]);
-        }
-        var iconBase = 'assets/img/icon/';
-        var createMarkerBus = function (bus) {
-
-            var marker = new google.maps.Marker({
-                map: map,
-                position: new google.maps.LatLng(bus.latitude, bus.longitude),
-                icon : iconBase+'icon-bus.png',
-                title: 'Maminasata 1'
-            });
-            marker.content = '' +
-                '<div class="infoWindowContent">' +
-                '   <h4>' + marker.title + '</h4>  ' +
-                '   <p><i class="fa fa-dashboard"></i> Keceptan : 30 km/jam</p>' +
-                '   <p><i class="fa fa-bus"></i> Status : OTW</p>' +
-                '   <p><i class="fa fa-clock-o"></i> ETA : 15 menit</p>' +
-                '</div>';
-            google.maps.event.addListener(marker, 'click', function () {
-                /*open map default content*/
-                infoWindow.setContent(marker.content);
-                infoWindow.open(map, marker);
-            });
-        };
-
-        var bus = {latitude:-5.138828,longitude:119.420517};
-        createMarkerBus(bus);
-    }
 });

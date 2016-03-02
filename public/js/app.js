48248509b31c6afb7853ce3ecce5bf9f794cbb58
Fadhli12 <thegenesispath@gmail.com>
2016-03-02 13:04:53 +0800

    progress tambah halte

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by virah on 17-Nov-15.
+ */
+
+var myApp = angular.module('myApp',['ui.router','ngAnimate','ngRoute']);
+
+myApp.config(function($stateProvider,$urlRouterProvider,$locationProvider,$routeProvider){
+   $stateProvider
+       .state('map',{
+           url:'/map/:id_koridor',
+           templateUrl:'public/app/view/map.html',
+           controller:'mapCtrl'
+       })
+       .state('koridor',{
+           url:'/koridor',
+           templateUrl:'public/app/view/koridor.html',
+           controller:'koridorCtrl'
+       });
+    $urlRouterProvider.otherwise('map/3');
+    //$locationProvider.html5Mode(true);
+
+})
